4d28cb46efd0092f8bca62dd7dd2e33c9edfb2f1
c0a250369c <c0a250369c@edu.teu.ac.jp>
2026-06-09 15:52:19 +0900

    練習７と８二枚目の画を打を反転させ、不連続にならないようにした

--- pygame_image.py
+++ pygame_image.py
@@ -10,16 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")# 練習3：こうかとんSurfaceの作成
     kk_img = pg.transform.flip(kk_img, True, False)# 練習3：こうかとんの左右反転
+    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         x = tmr
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(kk_img, [300, 200]) # 練習4：こうかとんSurfaceの描
+        screen.blit(bg_img, [-x, 0])# 練習5 :背景画像を右から左へ
+        screen.blit(bg_img2, [-x+1600, 0])#練習7 :
+        screen.blit(kk_img, [300, 200]) # 練習4：こうかとんSurfaceの描画
         pg.display.update()
         tmr += 1        
         clock.tick(200)
