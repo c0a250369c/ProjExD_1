d1808de7799ebe9630a9dacb77bec3859d761d45
c0a250369c <c0a250369c@edu.teu.ac.jp>
2026-06-09 17:03:24 +0900

    2.move_ipメソッド一回だけ

--- pygame_image.py
+++ pygame_image.py
@@ -4,7 +4,6 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
@@ -15,24 +14,30 @@ def main():
     kk_img = pg.transform.flip(kk_img, True, False)# 練習3：こうかとんの左右反転
     kk_rct = kk_img.get_rect()#練習10-1:こうかとんrectの取得
     kk_rct.center = 300, 200#練習10-2:こうかとんの中心座標を指定
-
-    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()#練習10-3
+        if key_lst[pg.K_UP] or key_lst[pg.K_DOWN] or key_lst[pg.K_LEFT] or key_lst[pg.K_RIGHT]:
 
-        if key_lst[pg.K_UP]:#上矢印キーが押されたら
-            kk_rct.move_ip(-1, -1)#上に移動
-        if key_lst[pg.K_DOWN]:    
-            kk_rct.move_ip(-1, +1)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2, 0)
-        kk_rct.move_ip(-1, 0)
+            if key_lst[pg.K_UP]:#上矢印キーが押されたら
+                muki1 = -1
+                muki2 = -1
+            if key_lst[pg.K_DOWN]:    
+                muki1 = -1
+                muki2 = +1
+            if key_lst[pg.K_LEFT]:
+                muki1 = -1
+                muki2 = 0
+            if key_lst[pg.K_RIGHT]:
+                muki1 = +2
+                muki2 = 0
+        else:
+            muki1 = -1
+            muki2 = 0
+        kk_rct.move_ip(muki1, muki2)
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])# 練習5 :背景画像を右から左へ
