7e065b26faa37bd65fe2fa104c3960a74c6e9b42
c0a250369c <c0a250369c@edu.teu.ac.jp>
2026-06-09 16:28:15 +0900

    練習10:こうかとんが移動できる

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,32 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")# 練習3：こうかとんSurfaceの作成
     kk_img = pg.transform.flip(kk_img, True, False)# 練習3：こうかとんの左右反転
+    kk_rct = kk_img.get_rect()#練習10-1:こうかとんrectの取得
+    kk_rct.center = 300, 200#練習10-2:こうかとんの中心座標を指定
+
     
+    # print(key_lst)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed()#練習10-3
+
+        if key_lst[pg.K_UP]:#上矢印キーが押されたら
+            kk_rct.move_ip(0, -1)#上に移動
+        if key_lst[pg.K_DOWN]:    
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
+
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])# 練習5 :背景画像を右から左へ
         screen.blit(bg_img2, [-x+1600, 0])#練習7 :
-        screen.blit(bg_img, [-x+3200, 0])#
-        screen.blit(kk_img, [300, 200]) # 練習4：こうかとんSurfaceの描画
+        screen.blit(bg_img, [-x+3200, 0])#練習9:
+        screen.blit(kk_img, kk_rct)#練習10-5:
         pg.display.update()
         tmr += 1        
         clock.tick(200)
